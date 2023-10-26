f5e704125fdb6a547f74e3070bdeef86969adeaf
al002 <al002@disroot.org>
2023-10-26 22:49:45 +0800

    2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,41 @@
-use serde_json;
+use serde_json::{self, Number};
 use std::env;
 
 // Available if you need it!
-// use serde_bencode
+// use serde_bencode;
+
+fn decode_string_value(encoded_value: &str) -> serde_json::Value {
+    // Example: "5:hello" -> "hello"
+    let colon_index = encoded_value.find(':').unwrap();
+    let number_string = &encoded_value[..colon_index];
+    let number = number_string.parse::<i64>().unwrap();
+    let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+    return serde_json::Value::String(string.to_string());
+}
+
+fn decode_integer_value(encoded_value: &str) -> serde_json::Value {
+    let i = &encoded_value[1..encoded_value.len() -  1];
+
+    let n: Number;
+    if i.chars().next().unwrap() == '-' {
+       n = Number::from(i.parse::<i64>().unwrap());
+    } else {
+       n = Number::from(i.parse::<u64>().unwrap());
+    }
+
+    return serde_json::Value::Number(n);
+}
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+    let next_char = encoded_value.chars().next().unwrap();
+    match next_char {
+        n if n.is_digit(10) => decode_string_value(encoded_value),
+        'i' => decode_integer_value(encoded_value),
+        _ => {
+            panic!("Unhandled encoded value: {}", encoded_value)
+        }
     }
 }
 
