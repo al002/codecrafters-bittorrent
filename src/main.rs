9e024fa6c3c473e7d965e987e3ea7cf38714a8a4
al002 <al002@disroot.org>
2023-10-27 00:20:43 +0800

    stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use serde_json::{self, Number};
+use serde_json::{self, Number, Value};
 use std::env;
 
 // Available if you need it!
@@ -14,16 +14,61 @@ fn decode_string_value(encoded_value: &str) -> serde_json::Value {
 }
 
 fn decode_integer_value(encoded_value: &str) -> serde_json::Value {
-    let i = &encoded_value[1..encoded_value.len() -  1];
+    let i = &encoded_value[1..encoded_value.len() - 1];
 
     let n: Number;
     if i.chars().next().unwrap() == '-' {
-       n = Number::from(i.parse::<i64>().unwrap());
+        n = Number::from(i.parse::<i64>().unwrap());
     } else {
-       n = Number::from(i.parse::<u64>().unwrap());
+        n = Number::from(i.parse::<u64>().unwrap());
     }
 
     return serde_json::Value::Number(n);
+}
+
+fn decode_list_value(encoded_value: &str) -> serde_json::Value {
+    let mut content = &encoded_value[1..encoded_value.len() - 1];
+
+    let mut v = vec![];
+
+    while let Some(next_char) = content.chars().next() {
+        if next_char == 'e' {
+            break;
+        }
+
+        match next_char {
+            n if n.is_digit(10) => {
+                let decoded = decode_string_value(content);
+                v.push(decoded.clone());
+                if let Value::String(s) = decoded {
+                    content = &content[s.len() + 2..];
+                }
+            }
+            'i' => {
+                let decoded = decode_integer_value(content);
+                v.push(decoded.clone());
+
+                if let Value::Number(n) = decoded {
+                    if n.is_i64() {
+                        if let Some(i) = n.as_i64() {
+                            content = &content[i.to_string().len() + 2..];
+
+                        }
+                    } else if n.is_u64() {
+                        if let Some(u) = n.as_i64() {
+                            content = &content[u.to_string().len() + 2..];
+
+                        }
+                    }
+                }
+            }
+            _ => {
+                panic!("Unhandled encoded value: {}", encoded_value);
+            }
+        }
+    }
+
+    return serde_json::Value::Array(v);
 }
 
 #[allow(dead_code)]
@@ -33,6 +78,7 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     match next_char {
         n if n.is_digit(10) => decode_string_value(encoded_value),
         'i' => decode_integer_value(encoded_value),
+        'l' => decode_list_value(encoded_value),
         _ => {
             panic!("Unhandled encoded value: {}", encoded_value)
         }
