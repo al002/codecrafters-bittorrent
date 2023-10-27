91c8f529f115dcd45afc28b8687f446a5c002d88
al002 <al002@disroot.org>
2023-10-27 14:28:55 +0800

    stage 4

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,37 @@
-use serde_json;
+use anyhow::{anyhow, Error, Ok, Result};
+use serde_json::{self, Map};
 use std::env;
-use anyhow::{Result, Ok, anyhow};
 
 // Available if you need it!
 // use serde_bencode;
 
-fn decode_string_value(encoded_value: &str) -> Result<(serde_json::Value, &str),  anyhow::Error> {
+fn decode_string_value(encoded_value: &str) -> Result<(serde_json::Value, &str), Error> {
     // Example: "5:hello" -> "hello"
 
     match encoded_value.split_once(":") {
         Some((size, rest)) => {
-            let size = size.parse::<usize>().map_err(|_| anyhow!("Invalid size: {}", size))?;
+            let size = size
+                .parse::<usize>()
+                .map_err(|_| anyhow!("Invalid size: {}", size))?;
 
             let (word, rest) = rest.split_at(size);
             return Ok((word.into(), rest));
-        },
+        }
         None => Err(anyhow!("Invalid bencode syntax: {}", encoded_value)),
     }
 }
 
-fn decode_integer_value(encoded_value: &str) -> Result<(serde_json::Value, &str),  anyhow::Error> {
+fn decode_integer_value(encoded_value: &str) -> Result<(serde_json::Value, &str), Error> {
     match encoded_value.split_at(1).1.split_once('e') {
         Some((n, rest)) => {
             let n = n.parse::<i64>()?;
             return Ok((serde_json::Value::Number(n.into()), rest));
-        },
+        }
         None => Err(anyhow!("Invalid bencode syntax: {}", encoded_value)),
     }
 }
 
-fn decode_list_value(encoded_value: &str) -> Result<(serde_json::Value, &str),  anyhow::Error> {
+fn decode_list_value(encoded_value: &str) -> Result<(serde_json::Value, &str), Error> {
     let mut rest = &encoded_value[1..];
 
     let mut v = vec![];
@@ -49,6 +51,55 @@ fn decode_list_value(encoded_value: &str) -> Result<(serde_json::Value, &str),  
     ))
 }
 
+fn decode_dictionary_value(encoded_value: &str) -> Result<(serde_json::Value, &str), Error> {
+    let mut rest = &encoded_value[1..];
+
+    let mut m = Map::new();
+    let mut v: Vec<serde_json::Value> = vec![];
+
+    if let Some(e) = rest.chars().next_back() {
+        if e != 'e' {
+            return Err(anyhow!(
+                "Invalid bencode syntax: dictionary not terminated with 'e'"
+            ));
+        }
+    }
+
+    while let Some(next_char) = rest.chars().next() {
+        if next_char == 'e' {
+            break;
+        }
+
+        let (decoded_value, new_rest) = decode_bencoded_value(rest)?;
+        v.push(decoded_value);
+        rest = new_rest;
+    }
+
+    let even_elements: Vec<serde_json::Value> = v
+        .clone()
+        .into_iter()
+        .enumerate()
+        .filter(|&(i, _)| i % 2 == 0)
+        .map(|(_, e)| e)
+        .collect();
+
+    let odd_elements: Vec<serde_json::Value> = v
+        .clone()
+        .into_iter()
+        .enumerate()
+        .filter(|&(i, _)| i % 2 != 0)
+        .map(|(_, e)| e)
+        .collect();
+
+    let pairs = even_elements.into_iter().zip(odd_elements);
+
+    for p in pairs {
+        m.insert(p.0.to_string(), p.1);
+    }
+
+    Ok((serde_json::Value::Object(m), &rest[1..]))
+}
+
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> Result<(serde_json::Value, &str)> {
     // If encoded_value starts with a digit, it's a number
@@ -57,6 +108,7 @@ fn decode_bencoded_value(encoded_value: &str) -> Result<(serde_json::Value, &str
         n if n.is_digit(10) => decode_string_value(encoded_value),
         'i' => decode_integer_value(encoded_value),
         'l' => decode_list_value(encoded_value),
+        'd' => decode_dictionary_value(encoded_value),
         _ => Err(anyhow!("Invalid bencode syntax: {}", encoded_value)),
     }
 }
